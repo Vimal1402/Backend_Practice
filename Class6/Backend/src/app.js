b24d0f4d0fa340c7d8062ef63f019d140e46ab22
Vimal1402 <vimalkumar996426@gmail.com>
2026-02-01 20:05:03 +0530

    feat: initialize frontend with React and Vite setup
    - Added package.json with dependencies and scripts for development
    - Included Vite logo in public directory
    - Created main application component (App.jsx) with initial state and API call
    - Added React logo in assets directory
    - Defined global styles in index.css
    - Set up entry point in main.jsx for rendering the application
    - Configured Vite with react plugin in vite.config.js

--- Class6/Backend/src/app.js
+++ Class6/Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
-const noteModel = require('./models/note.model')
+const noteModel = require('./models/note.model');
+const cors = require('cors')
 
+const app = express();
 app.use(express.json())
+app.use(cors())
 
 // Creating API -- POST
 app.post('/api/notes', async (req, res) =>{
